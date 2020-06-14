1ef7f0a97dfc92612c936fe2b95068ec7a971e64
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-14 19:26:10 -0300

    add ContentButton style

---
+++ src/Components/styles/ContentButtons.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const ContentButtons = styled.div`
+    display:flex;
+    flex-direction:row;
+    flex-wrap:wrap;
+    width:200px;
+`
+
+export default ContentButtons
