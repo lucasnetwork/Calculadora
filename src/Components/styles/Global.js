4207e702665875b2626b46687e35950c5a01ea93
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-14 19:37:42 -0300

    add global style

---
+++ src/Components/styles/Global.js
@@ -0,0 +1,21 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    html,body,#root{
+        height:100vh;
+        width:100%;
+    }
+    .App{
+        display:flex;
+        justify-content:space-around;
+        align-items:center;
+        height:100%;
+    }
+`
+
+export default GlobalStyle
