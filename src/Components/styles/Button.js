a7bdf0d9d043ead0054b43e345a8d04b721cf410
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-15 16:18:13 -0300

    add transition hover in buttons

--- src/Components/styles/Button.js
+++ src/Components/styles/Button.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
+    background-color:#fff;
+    box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.2);
     &.number{
         height:60px;    
     }
+    outline:none;
     width:50px;
     height:36px;
     text-align:center;
     border:none;
+    transition: filter 0.3s;
+    :hover{
+        filter:brightness(95%)
+    };
+    :active{
+        filter:brightness(75%)
+    }
 `
 
 export default Button
