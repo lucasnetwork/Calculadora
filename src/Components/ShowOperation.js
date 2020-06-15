1eaf75c85283b31b37b6ee52b43ec64b0fdfa650
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-15 16:19:59 -0300

    add styles in content operations

--- src/Components/ShowOperation.js
+++ src/Components/ShowOperation.js
@@ -1,14 +1,15 @@
 import React,{useEffect} from 'react'
+import ShowOperationContent from './styles/ShowOperationContent'
 
 const ShowOperation = ({currentValue,currentOperation,setCurrentOperation}) =>{
     useEffect(() =>{
         
     },[currentOperation])    
     return(
-        <>
+        <ShowOperationContent>
             <div>{currentValue}</div>
             <div>{currentOperation}</div>
-        </>
+        </ShowOperationContent>
     )
 }
 
