d5ebdcdf26b10d17da15a816db615ba45c99f4c6
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-15 16:17:05 -0300

    add contentOperation style

---
+++ src/Components/styles/ShowOperationContent.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ShowOperationContent = styled.div`
+    display:flex;
+    justify-content:space-between;
+    box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.2);
+    height:50px;
+    align-items:center;
+    padding:10px;
+    margin-bottom:2px;
+`
+
+export default ShowOperationContent
