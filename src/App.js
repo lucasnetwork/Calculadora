c15df13b85aab320444058940a3112ef36204aeb
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-15 14:06:42 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React,{useState} from 'react';
 import ContentButtons from './Components/styles/ContentButtons'
 import Buttons from './Components/Buttons'
+import ShowOperation from './Components/ShowOperation'
 import GlobalStyle from './Components/styles/Global'
 
 function App() {
+  const [currentValue,setCurrentValue] = useState('0')
+  const [previousValue,setPreviousValue] = useState('0')
+  const [currentOperation,setCurrentOperation] = useState('')
   return (
     <>
       <GlobalStyle/>
       <div className="App">
-
-        <ContentButtons>
-          <Buttons/>
-        </ContentButtons>
+        <div className="content">
+          <ShowOperation 
+            currentValue={currentValue} 
+            currentOperation={currentOperation} 
+            setCurrentOperation={setCurrentOperation}
+          />
+          <ContentButtons >
+            <Buttons 
+              setCurrentValue={setCurrentValue} 
+              setCurrentOperation={setCurrentOperation}
+              previousValue={previousValue}
+              setPreviousValue={setPreviousValue}
+              currentOperation={currentOperation}
+              currentValue={currentValue}
+            />
+          </ContentButtons>
+        </div>
       </div>
     </>
   );
